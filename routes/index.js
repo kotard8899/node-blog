1483da633535ccaacecf1830c78fb756d71603cc
ryan <sally41176789@yahoo.com.tw>
2020-07-26 15:29:46 +0800

    完成文章種類過濾

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,6 @@ router.get('/', function(req, res, next) {
     const categories = snapshot.val();
     //pagination
     ({articles, pages} = convertPagination(articles, req.query.page));
-    
     res.render('index', { 
       articles,
       categories,
@@ -33,6 +32,83 @@ router.get('/', function(req, res, next) {
     });
   })
 });
+
+router.get('/:category', function(req, res, next) {
+  let articles = [];
+  let categories = {};
+  let pages = {};
+  const categoryPath = req.param('category') || '';
+  let categoryId = '';
+  categoriesRef.once('value').then((snapshot) => {
+    categories = snapshot.val();
+    //取得種類ID
+    if(categoryPath) {
+      snapshot.forEach((child) => {
+        if(child.val().path === categoryPath) {
+          categoryId = child.val().id;
+        }
+      })
+    }
+    return articlesRef.orderByChild('update_time').once('value')
+  }).then((snapshot) => {
+    snapshot.forEach((child) => {
+      if(child.val().status === 'public') {
+        articles.push(child.val());
+      }
+    })
+    articles.reverse();
+    //過濾種類文章
+    if(categoryId) {
+      articles = articles.filter((article) => article.category === categoryId)
+    }
+
+    //pagination
+    ({articles, pages} = convertPagination(articles, req.query.page));
+    res.render('index', { 
+      articles,
+      categories,
+      moment,
+      striptags,
+      pages
+    });
+  })
+})
+// router.get('/:category', function(req, res, next) {
+//   let articles = [];
+//   let pages = {};
+//   const categoryPath = req.param('category') || '';
+//   console.log({categoryPath})
+//   let categoryId = '';
+//   articlesRef.orderByChild('update_time').once('value').then((snapshot) => {
+//     snapshot.forEach((child) => {
+//       if(child.val().status === 'public') {
+//         articles.push(child.val());
+//       }
+//     })
+//     articles.reverse();
+//     return categoriesRef.once('value');
+//   }).then((snapshot) => {
+//     const categories = snapshot.val();
+//     //pagination
+//     ({articles, pages} = convertPagination(articles, req.query.page));
+//     //過濾種類文章
+//     if(categoryPath) {
+//       snapshot.forEach((child) => {
+//         if(child.val().path === categoryPath) {
+//           categoryId = child.val().id;
+//         }
+//       })
+//     }
+//     res.render('index', { 
+//       articles,
+//       categories,
+//       moment,
+//       striptags,
+//       pages
+//     });
+//   })
+// });
+
 router.get('/post/:id', function(req, res, next) {
   const id = req.param('id');
   let article = {}
